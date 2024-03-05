74fd01f3913f1e2f043f77ba80d4bb7fb71962ad
Felipe Nascimento <felipe1.jn@hotmail.com>
2024-03-04 22:09:03 -0300

    adicionando chamada api do express antes do metodo render

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+constructor(props) {
+  super(props);
+  this.state = { apiResponse: "" };
+}
+
+callAPI() {
+  fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+  this.callAPI();
+}
+
 root.render(
   <React.StrictMode>
     <App />
